fed98b81e7b82c097ad6e46334ff7555dc53a008
Yurii Litovinskyi <y.litovinskyi@gmail.com>
2020-02-10 17:06:33 +0200

    Updates

--- precache-manifest.919a9e83f49b732ddd60e1ac1c2566d8.js
+++ precache-manifest.919a9e83f49b732ddd60e1ac1c2566d8.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4ee5df3c664451eabb0f97cabbf3f3ca",
+    "revision": "f62dfa34542a6c30b040fcbb65d0fc22",
     "url": "/test-task-react-yarandin/index.html"
   },
   {
-    "revision": "d775350de4cee2cc1532",
+    "revision": "c24c593d469c6c87d60b",
     "url": "/test-task-react-yarandin/static/css/2.2623327d.chunk.css"
   },
   {
-    "revision": "1dfb9d878503fd0d27aa",
+    "revision": "4eebcb3ed7d0e5a5f6b6",
     "url": "/test-task-react-yarandin/static/css/main.dc00c781.chunk.css"
   },
   {
-    "revision": "d775350de4cee2cc1532",
-    "url": "/test-task-react-yarandin/static/js/2.68c8eff4.chunk.js"
+    "revision": "c24c593d469c6c87d60b",
+    "url": "/test-task-react-yarandin/static/js/2.806cde91.chunk.js"
   },
   {
     "revision": "928d7b5eb39d16fe9a880722c974b51e",
-    "url": "/test-task-react-yarandin/static/js/2.68c8eff4.chunk.js.LICENSE.txt"
+    "url": "/test-task-react-yarandin/static/js/2.806cde91.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1dfb9d878503fd0d27aa",
-    "url": "/test-task-react-yarandin/static/js/main.91cec821.chunk.js"
+    "revision": "4eebcb3ed7d0e5a5f6b6",
+    "url": "/test-task-react-yarandin/static/js/main.d71ba874.chunk.js"
   },
   {
     "revision": "648bc1de212b434edcfd",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/test-task-react-yarandin/precache-manifest.f039b22162cd44bbf01423563cf3eb6b.js"
+  "/test-task-react-yarandin/precache-manifest.919a9e83f49b732ddd60e1ac1c2566d8.js"
 );
 
 self.addEventListener('message', (event) => {
