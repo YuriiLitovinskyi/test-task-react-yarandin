76be158b40018cb9bc9380bb550ec794994bdc2a
YuriiLitovinskyi <y.litovinskyi@gmail.com>
2020-05-24 10:23:37 +0300

    removed regex

--- src/actions/FilmsActions.js
+++ src/actions/FilmsActions.js
@@ -26,10 +26,8 @@ export const getFilms = () => async (dispatch) => {
 
 export const getSingleFilm = (film) => async (dispatch) => {
 
-    const filmUrl = film.url.replace(/^http?\//i, "https");
-
     try {
-        const responce = await fetch(`${filmUrl}`);
+        const responce = await fetch(`${film.url}`);
         const singleFilm = await responce.json();         
     
         dispatch({
